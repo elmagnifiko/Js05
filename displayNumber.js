868c4248f35808144a7a9a190f0034e5909d67fc
elmagnifiko <121030644+elmagnifiko@users.noreply.github.com>
2023-02-19 12:36:59 +0100

    Update displayNumber.js

--- displayNumber.js
+++ displayNumber.js
@@ -1,10 +1,10 @@
 
 function displayNumber()
 {
-    let tableau = [1,2,3,4,5,6,7,8,9,10];
+    let nombres = [1,2,3,4,5,6,7,8,9,10];
 
-    for (let i=0; i<tableau.length; i++){
-        console.log(tableau[i]);
+    for (let i=0; i<nombres.length; i++){
+        console.log(nombres[i]);
     }
 }
 displayNumber();
